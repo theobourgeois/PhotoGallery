f52231807634e9486c4a0197b4b05302be9add51
theobourgeois <theoobourgeois@gmail.com>
2024-11-15 13:26:42 -0400

    fix photo proportions

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,12 @@
 
-
-
-
 function App() {
   return (
     <main className="w-screen h-max">
       <section className="p-12">
-        <div className="grid grid-cols-2">
+        <div className="grid sm:grid-cols-2 grid-cols-1">
           {Array(24).fill(0).map((_, index) => (
-            <div className="h-full">
-              <img className="h-full" key={index} src={`/${index + 1}.JPG`} />
+            <div className="object-cover">
+              <img className="object-cover h-full" key={index} src={`/${index + 1}.JPG`} />
             </div>
           ))}
         </div>
